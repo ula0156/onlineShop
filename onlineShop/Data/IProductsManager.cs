d97734a8b4276e2b850951843d3bd5e1bfa00e4d
Iuliia Aleksandrova <julie.aleksandrova@gmail.com>
2017-07-19 13:04:06 -0700

    Adding Interface for product managing and implementation for in-memmory database.

---
+++ onlineShop/Data/IProductsManager.cs
@@ -0,0 +1,15 @@
+﻿using OnlineShop.Products;
+using System;
+
+namespace onlineShop.Data
+{
+    /// <summary>
+    /// Interface used for read and write access to the database of products.
+    /// </summary>
+    interface IProductsManager : IProductsReader
+    {
+        bool TryAddProduct(Product product, int stock);
+
+        bool TryRemoveProduct(Guid productId);
+    }
+}
---
+++ onlineShop/Data/IProductsReader.cs
@@ -0,0 +1,17 @@
+﻿using OnlineShop.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace onlineShop.Data
+{
+    /// <summary>
+    /// Interface used for read only access to the database of products.
+    /// </summary>
+    interface IProductsReader
+    {
+        IQueryable<Product> GetProducts();
+
+        int GetProductStock(Guid productId);
+    }
+}
