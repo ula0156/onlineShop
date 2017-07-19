b6da6b29dbed43b42a9d6b78ea4f1f9e4a98280d
Iuliia Aleksandrova <julie.aleksandrova@gmail.com>
2017-07-19 13:25:53 -0700

    Adding Interface for stocks managing and implementation for it

---
+++ onlineShop/Data/IStocksManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace onlineShop.Data
+{
+    public interface IStocksManager
+    {
+        bool TryIncreaseStock(Guid productId);
+
+        bool TryDecreaseStock(Guid productId);
+    }
+}
